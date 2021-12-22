174d39086e857105a254648895f5dfc11b0c0322
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2021-12-22 23:28:25 +0000

    add conditional and print accordingly

--- race_register.js
+++ race_register.js
@@ -11,3 +11,14 @@ let runnerAge = 19;
 if (runnerAge > 18 && registeredEarly) {
   raceNumber += 1000;
 } 
+
+// Print message according to age and registration number of runner
+if (runnerAge > 18 && registeredEarly) {
+  console.log(`Your race number is ${raceNumber} and you will race at 9.30am.`);
+} else if (runnerAge > 18 && !registeredEarly) {
+  console.log(`Your race number is ${raceNumber}. You are over 18, but registered late, so you will race at 11:00am.`);
+} else if (runnerAge < 18) {
+  console.log(`Your race number is ${raceNumber}. You are under 18, so you will race at 12:30pm.`);
+} else {
+  console.log('Ah, you\'re 18. Please see registration desk.');
+}
