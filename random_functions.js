b3281602e0df5780368f739ab101238f2656a011
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2021-12-26 17:15:16 +0000

    add toEmoticon() function

--- random_functions.js
+++ random_functions.js
@@ -41,3 +41,28 @@ const tipCalculator = (quality, total) => {
     return tip;
   }
 }
+
+// Function 3: input a string which relates to an emotion and action and get an emoji:
+
+const toEmoticon = (emotion) => {
+  switch (emotion) {
+    case 'shrug':
+      return '|_{"}_|';
+      break;
+    case 'smiley face':
+      return ':)';
+      break;
+    case 'frowny face':
+      return ':(';
+      break;
+    case 'winky face':
+      return ';)';
+      break;
+    case 'heart':
+      return '<3';
+      break;
+    default:
+      return '|_(* ~ *)_|';
+      break;
+  }
+}
