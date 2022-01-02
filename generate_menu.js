f8080fc0e0efabb579849efc6d1c95c1ff830632
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-01-02 21:35:45 +0000

    adding courses to arrays in _courses
    + adding comments

--- generate_menu.js
+++ generate_menu.js
@@ -5,6 +5,8 @@ random index. It also has getter and setter methods. */
 /* Note the setters involve changing a menu item's name. To delete an item, it's probably easier to use .unshift() or .pop() (or another relevant method), rather than
 use the setter to change the price.*/
 
+// _courses not to be changed directly, hence the prepended '_'
+
 let menu = {
   _courses: {
     appetisers: [],
@@ -65,4 +67,24 @@ let menu = {
     ${dessertName}
     The total price is $${totPrice}`;
   }
+  
+// Adding appetisers:
+menu.addDishToCourse('appetisers', 'Bruschetta', 4.50);
+menu.addDishToCourse('appetisers', 'Broccoli and Stilton Soup', 3.50);
+menu.addDishToCourse('appetisers', 'Sushi', 7.50);
+
+// Adding mains:
+menu.addDishToCourse('mains', 'Margherita Pizza', 10.00);
+menu.addDishToCourse('mains', 'Lobster Ravioli', 16.95);
+menu.addDishToCourse('mains', 'Beef Wellington', 19.00);
+
+// Adding desserts
+menu.addDishToCourse('desserts', 'Tiramisu', 8.95);
+menu.addDishToCourse('desserts', 'Cheese Board', 4.95);
+menu.addDishToCourse('desserts', 'Eton Mess', 10.50);
+
+// Logging a randomly generated three-course meal:
+console.log(menu.generateRandomMeal());
 }
+
+
