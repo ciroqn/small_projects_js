bff87e20e763928ad404e8ac5efae7be743b7823
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-01-02 21:19:47 +0000

    create generate_menu.js

---
+++ generate_menu.js
@@ -0,0 +1,3 @@
+/*This project creates a randomly generated three-course menu with total price attached. The mechanism is an object which has three courses (arrays within objects)
+and several functions (methods) which add dishes and prices to certain courses (i.e. appetisers, mains, desserts), and generates a three-course menu based on a 
+random index. It also has getter and setter methods. */
