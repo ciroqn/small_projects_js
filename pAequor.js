b5b6926a6051b6ef06c96c4428cba988b49cc6e0
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-01-13 18:20:05 +0000

    excise typo

--- pAequor.js
+++ pAequor.js
@@ -1,4 +1,4 @@
-/*This project has a fadctory function which creates instances of P. aequor specimens, each with a specimen number and a mock DNA array with 15 bases. Each 
+/*This project has a factory function which creates instances of P. aequor specimens, each with a specimen number and a mock DNA array with 15 bases. Each 
 specimen has numerous functions to mimic mutations in their environment. DNA can also be compared between specimens. A work in progress that can definitely be
 improved.*/
 
@@ -95,3 +95,6 @@ console.log('\n');
 console.log(pAequorSpec1);
 console.log('\n');
 console.log(pAequorSpec.compareDNA(pAequorSpec1));
+
+
+
