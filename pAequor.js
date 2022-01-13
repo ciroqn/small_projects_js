b68d15728703e2e83e9b3e5ec76256c3bc9ce649
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-01-13 16:10:39 +0000

    pAequor intro

---
+++ pAequor.js
@@ -0,0 +1,2 @@
+/*This project has a fadctory function which creates instances of P. aequor specimens, each with a specimen number and a mock DNA array with 15 bases. Each 
+specimen has numerous functions to mimic mutations in their environment. DNA can also be compared between specimens:*/
